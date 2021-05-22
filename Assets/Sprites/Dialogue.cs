b1a6a420d13caf754d9143578e2406d11d63d445
Gustavo Cirilo <servicobrq735919@userede.com.br>
2021-05-22 14:03:39 -0300

    Melhorando caixas de diálogo

---
+++ Assets/Sprites/Dialogue.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+
+    public Sprite[] profiles;
+    public string[] speechText;
+    public string actorName;
+
+    public float radius;
+    public LayerMask playerPlayer;
+
+    private bool onRadios;
+    private bool interacting;
+    public DialogueControl dialogueControl;
+
+    private void FixedUpdate()
+    {
+        Interact();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && !dialogueControl.gameObject.activeSelf && onRadios)
+        {
+            dialogueControl.Speech(profiles, speechText, actorName);
+        }
+    }
+
+
+    public void Interact()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radius, playerPlayer);
+
+        if(hit != null)
+        {
+            onRadios = true;
+ 
+        }
+        else
+        {
+            onRadios = false;
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+}
---
+++ Assets/Sprites/DialogueControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueControl : MonoBehaviour
+{
+
+    [Header("Components")]
+    public GameObject dialogueObj;
+    public Image profile;
+    private Sprite[] profiles;
+    public TMP_Text speechText;
+    public TMP_Text actorNameText;
+
+    [Header("Settings")]
+    public float typingSpeed;
+    private string[] sentences;
+    private int index;
+
+    public void Speech(Sprite[] sprites, string[] txt, string actorName)
+    {
+        dialogueObj.SetActive(true);
+        sentences = txt;
+        actorNameText.text = actorName;
+        profiles = sprites;
+        StartCoroutine(TypeSentences());
+
+    }
+
+    IEnumerator TypeSentences()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            profile.sprite = profiles[index];
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+          if(speechText.text == sentences[index])
+        {
+            if(index < sentences.Length - 1)
+            {
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentences());
+            } else
+            {
+                speechText.text = "";
+                index = 0;
+                dialogueObj.SetActive(false);
+            }
+        }
+
+    }
+
+
+
+}
